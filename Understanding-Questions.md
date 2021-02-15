9a9e9a9a02e0ee5866665f6cc1ab0921c52333f9
Joshua Samaniego- Evans <joshuasamaniego-evans@Joshuas-MacBook-Pro.local>
2021-02-15 15:19:34 -0700

    meets mvp all buttons working with useReducer hook

--- Understanding-Questions.md
+++ Understanding-Questions.md
@@ -1,7 +1,13 @@
 # Understanding Questions:
-1. What are the steps of execution from the pressing of the 1 button to the rendering of our updated value? List what part of the code excutes for each step.
-* The user presses the 1 button.
-* 
-...
 
-* TotalDisplay shows the total plus 1.
+1. What are the steps of execution from the pressing of the 1 button to the rendering of our updated value? List what part of the code excutes for each step.
+
+- The user presses the 1 button.
+- The onClick event gets triggered by that userEvent, which then, the onClick references our event listener of handleAdd, which in turn calls our dispatch function we get from the useReducer hook.
+- The dispatch function takes an object as an argument, which in this case (hehehe), is our action creator helper function of addOne(). We get access to this via our import for our actions folder.
+- All the addOne function does right now is return an object that almost acts like an actual "key", via the type key in that object (ex: {type(key): "something"(value)}).
+- Now that we have our action-specific object, dispatch takes that to our reducer and now a certain case in our reducer function switch statement, will modify and update our state in a way that we envision, based on the way our User interacts with our app.
+- In our case, with the action creator of addOne returning an object with a "type" key and an "ADD_ONE" value, our reducer's cases will now get triggered accordingly based off of "action.type", (which is one of our arguments that our reducer expects in order to function).
+- Our reducer's "ADD_ONE" case, then returns another object of a copy of our state, AND the modification we want to see, which in this case (hahah) is increasing our total (in state) to be whatever the current state of total is, plus 1.
+- After our reducer updates state with the new information/data we want, React triggers a re-render of our component(s), (since state now has been updated), and now our component of TotalDisplay has the appropriate data and finally,
+- TotalDisplay shows the total plus 1 (on our DOM).
--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useReducer } from 'react';
+import reducer, {initialState} from './reducers';
+import { applyNumber, changeOperation, clearDisplay, memoryAdd, memoryRetrieve, memoryClear } from './actions';
 
 import './App.css';
 
@@ -6,6 +8,33 @@ import TotalDisplay from './components/TotalDisplay';
 import CalcButton from './components/CalcButton';
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  // console.log("State: ", state);
+  // console.log("Dispatch: ", dispatch);
+
+  const handleCalc = (number) => {
+    dispatch(applyNumber(number));
+  }
+
+  const handleOperation = (operator) => {
+    dispatch(changeOperation(operator));
+  }
+
+  const handleClear = () => {
+    dispatch(clearDisplay());
+  }
+
+  const handleAddMemory = () => {
+    dispatch(memoryAdd());
+  }
+
+  const handleMemoryUpdate = () => {
+    dispatch(memoryRetrieve());
+  }
+
+  const handleClearMemory = () => {
+    dispatch(memoryClear());
+  }
 
   return (
     <div className="App">
@@ -17,44 +46,44 @@ function App() {
         <div className="col-md-12 d-flex justify-content-center">
           <form name="Cal">
             
-            <TotalDisplay value={0}/>
+            <TotalDisplay value={state.total}/>
             <div className="row details">
-              <span id="operation"><b>Operation:</b> X</span>
-              <span id="memory"><b>Memory:</b> 0</span>
+              <span id="operation"><b>Operation:</b>{state.operation}</span>
+              <span id="memory"><b>Memory:</b>{state.memory}</span>
             </div>
             
             <div className="row">
-              <CalcButton value={"M+"}/>
-              <CalcButton value={"MR"}/>
-              <CalcButton value={"MC"}/>
+              <CalcButton onClick={() => handleAddMemory()} value={"M+"}/>
+              <CalcButton onClick={() => handleMemoryUpdate()} value={"MR"}/>
+              <CalcButton onClick={() => handleClearMemory()} value={"MC"}/>
             </div>
 
             <div className="row">
-              <CalcButton value={1}/>
-              <CalcButton value={2}/>
-              <CalcButton value={3}/>
+              <CalcButton onClick={() => handleCalc(1)} value={1}/>
+              <CalcButton onClick={() => handleCalc(2)} value={2}/>
+              <CalcButton onClick={() => handleCalc(3)} value={3}/>
             </div>
 
             <div className="row">
-              <CalcButton value={4}/>
-              <CalcButton value={5}/>
-              <CalcButton value={6}/>
+              <CalcButton onClick={() => handleCalc(4)} value={4}/>
+              <CalcButton onClick={() => handleCalc(5)} value={5}/>
+              <CalcButton onClick={() => handleCalc(6)} value={6}/>
             </div>
 
             <div className="row">
-              <CalcButton value={7}/>
-              <CalcButton value={8}/>
-              <CalcButton value={9}/>
+              <CalcButton onClick={() => handleCalc(7)} value={7}/>
+              <CalcButton onClick={() => handleCalc(8)} value={8}/>
+              <CalcButton onClick={() => handleCalc(9)} value={9}/>
             </div>
 
             <div className="row">
-              <CalcButton value={"+"}/>
-              <CalcButton value={"*"}/>
-              <CalcButton value={"-"}/>
+              <CalcButton onClick={() => handleOperation("+")} value={"+"}/>
+              <CalcButton onClick={() => handleOperation("*")} value={"*"}/>
+              <CalcButton onClick={() => handleOperation("-")} value={"-"}/>
             </div>
 
             <div className="row ce_button">
-              <CalcButton value={"CE"}/>
+              <CalcButton onClick={() => handleClear()} value={"CE"}/>
             </div>
 
           </form>
